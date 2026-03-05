b74d2f58371c1a3308e329fceb44528e0765bebe
stennu420 <stenver.lindmaa@gmail.com>
2026-03-05 21:08:26 +0200

    StudyGroupController Index meetod

---
+++ Techno-uus/Controllers/StudyGroupController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static Techno_uus.Models.StudyGroup;
+
+namespace Techno_uus.Controllers
+{
+    public class StudyGroupController : Controller
+    {
+        // Siin on Index 
+      public async Task<IActionResult> Index()
+      {
+            var studyGroups = await _context.StudyGroups
+                .Include(s => s.LeaderPupil)
+                .OrderBy(s => s.Name)
+                .ToListAsync();
+
+            return View(studyGroups);
+
+      } 
+    }
+}
