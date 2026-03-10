c0d0a363a975235fe27eab3a472f7d8491d165fa
stennu420 <stenver.lindmaa@gmail.com>
2026-03-10 23:10:49 +0200

    StudyGroupController Create

--- Techno-uus/Controllers/StudyGroupController.cs
+++ Techno-uus/Controllers/StudyGroupController.cs
@@ -21,6 +21,27 @@ namespace Techno_uus.Controllers
       {
             _context = context;
       }
+        // Siin on Create
+        [HttpGet]
+        public IActionResult Cretae()
+        {
+            ViewBag.LeaderPupilId = new SelectList(_context.Pupils, "Id", "FirstName");
+            return View("CreateEdit");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,GroupName,StudyStart,StudyEnd,LeaderPupilId,ClassroomInfo,Level")] StudyGroup studyGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(studyGroup);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.LeaderPupilId = new SelectList(_context.Pupils, "Id", "FirstName", studyGroup.LeaderPupilId);
+            return View("CreateEdit", studyGroup);
+        }
+        
 
     }
 }
