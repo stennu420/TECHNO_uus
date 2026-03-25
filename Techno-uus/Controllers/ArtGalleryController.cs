18ef1bacce455c5db11081f5d26254e650ee8a86
Stenver <stenver.lindmaa@gmail.com>
2026-03-25 18:10:19 +0200

    ArtGalleryController,Index

---
+++ Techno-uus/Controllers/ArtGalleryController.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Techno_uus.Controllers
+{
+    public class ArtGalleryController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ArtGalleryController(AppDbContext context) 
+        {
+            _context = context;
+        }
+
+        //Siin algab Index
+        public  IActionResult Index() 
+        {
+            var artworks = _context.Artworks
+                .OrderByDescendig(artworks => artworks.CreationDate)
+                .ToList();
+
+            return View(artworks);
+        }
+
+        //Siin algab Create 
+        public  IActionResult Create() 
+        {
+            return View();
+        }
+
+        //Siin algab Create POST
+        [HttpPost]
+        public async
+    }
+}
---
+++ Techno-uus/Models/ArtGallery.cs
@@ -0,0 +1,21 @@
+﻿namespace Techno_uus.Models
+{
+    public class ArtGallery
+    {
+        public int Id { get; set; }
+
+        public string PupilName { get; set; }
+
+        public string? PupilId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public DateOnly CreationDate { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime SubmittedAt { get; set; }
+    }
+}
